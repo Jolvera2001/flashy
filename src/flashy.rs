ac91f61a4b0bb5ac9b78f23974452d818d78e8db
Jolvera2001 <jolvera6@stedwards.edu>
2025-09-12 16:25:24 -0500

    setup db connection

--- src/flashy.rs
+++ src/flashy.rs
@@ -1,28 +1,16 @@
 use eframe::{App, Frame};
 use egui::Context;
 use poll_promise::Promise;
-use sqlx::sqlite::SqlitePoolOptions;
 use sqlx::SqlitePool;
 use crate::flashy_events::FlashyEvents;
 
 pub struct Flashy {
     db_pool: SqlitePool,
-
     pub current_operation: Option<Promise<FlashyEvents>>
 }
 
 impl Flashy {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let runtime = tokio::runtime::Runtime::new().unwrap();
-
-        let db_pool = runtime.block_on(async {
-            let pool = SqlitePool::connect("sqlite:test.db").await.unwrap();
-
-            // run migrations here
-
-            pool
-        });
-
+    pub fn new(cc: &eframe::CreationContext<'_>, db_pool: SqlitePool) -> Self {
         Self {
             db_pool,
             current_operation: None
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate core;
 
-use eframe::egui;
 use crate::flashy::Flashy;
+use eframe::egui;
+use sqlx::SqlitePool;
+use std::env;
+use std::str::FromStr;
 
 mod flashy;
 mod flashy_events;
@@ -13,10 +16,17 @@ async fn main() -> Result<(), eframe::Error> {
         viewport: egui::ViewportBuilder::default().with_inner_size([400.0, 600.0]),
         ..Default::default()
     };
+    let db_options = sqlx::sqlite::SqliteConnectOptions::from_str("test.db")
+        .expect("Issue Creating Database")
+        .create_if_missing(true);
+
+    let pool = SqlitePool::connect_with(db_options).await.unwrap();
+
+    // run migrations here
 
     eframe::run_native(
         "Flashy",
         options,
-        Box::new(|cc| Ok(Box::new(Flashy::new()))),
+        Box::new(|cc| Ok(Box::new(Flashy::new(cc, pool)))),
     )
 }
