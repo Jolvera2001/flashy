2dd1d48a8c67180e0749aa241d60e31fae6860fc
Jolvera2001 <jolvera6@stedwards.edu>
2025-09-12 16:38:27 -0500

    small updates to startup

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,55 @@ mod flashy_events;
 mod models;
 
 #[tokio::main]
-async fn main() -> Result<(), eframe::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([400.0, 600.0]),
         ..Default::default()
     };
-    let db_options = sqlx::sqlite::SqliteConnectOptions::from_str("test.db")
-        .expect("Issue Creating Database")
-        .create_if_missing(true);
+    let db_options =
+        sqlx::sqlite::SqliteConnectOptions::from_str("sqlite:test.db")?.create_if_missing(true);
 
-    let pool = SqlitePool::connect_with(db_options).await.unwrap();
-
-    // run migrations here
+    let pool = SqlitePool::connect_with(db_options).await?;
+    run_migrations(&pool).await?;
 
     eframe::run_native(
         "Flashy",
         options,
         Box::new(|cc| Ok(Box::new(Flashy::new(cc, pool)))),
+    )?;
+
+    Ok(())
+}
+
+async fn run_migrations(pool: &SqlitePool) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS users (
+            id TEXT PRIMARY KEY,
+            date_created TEXT NOT NULL,
+            date_updated TEXT NOT NULL,
+            name TEXT NOT NULL
+        )
+    "#,
     )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS recurrences (
+            id TEXT PRIMARY KEY,
+            date_created TEXT NOT NULL,
+            date_updated TEXT NOT NULL,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            amount TEXT NOT NULL,
+            circulating_date TEXT NOT NULL
+        )
+    "#,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
 }
