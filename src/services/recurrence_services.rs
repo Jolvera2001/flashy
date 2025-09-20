a650a4aaf099f5824e218ad0cde135af1f15c378
Jolvera2001 <jolvera6@stedwards.edu>
2025-09-20 00:26:03 -0500

    first implementation for recurrence CRUD

--- src/services/recurrence_services.rs
+++ src/services/recurrence_services.rs
@@ -1,7 +1,7 @@
 use crate::models::recurrence::Recurrence;
 use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
-use sqlx::{Encode, Error, SqlitePool};
+use sqlx::{Error, SqlitePool};
 use uuid::Uuid;
 
 pub async fn create_recurrence(
@@ -30,23 +30,46 @@ pub async fn create_recurrence(
 }
 
 pub async fn get_recurrences(pool: &SqlitePool, user_id: &Uuid) -> Result<Vec<Recurrence>, Error> {
-    let recurrences = sqlx::query_as::<_, Recurrence>("SELECT * FROM recurrences WHERE user_id = ?")
-        .bind(&user_id)
-        .fetch_all(pool)
-        .await?;
+    let recurrences =
+        sqlx::query_as::<_, Recurrence>("SELECT * FROM recurrences WHERE user_id = ?")
+            .bind(&user_id)
+            .fetch_all(pool)
+            .await?;
 
     Ok(recurrences)
 }
 
-pub async fn get_recurrence_single(id: &Uuid) {}
+pub async fn get_recurrence_single(pool: &SqlitePool, id: &Uuid) -> Result<Recurrence, Error> {
+    let recurrence = sqlx::query_as::<_, Recurrence>("SELECT * FROM recurrences WHERE id = ?")
+        .bind(id)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(recurrence)
+}
 
 pub async fn update_recurrence(
-    user_id: Uuid,
+    pool: &SqlitePool,
     name: String,
     description: String,
     amount: Decimal,
     circulating_date: DateTime<Utc>,
-) {
+) -> Result<(), Error> {
+    sqlx::query("UPDATE recurrences SET name = ?, description = ?, amount = ?, circulating_date = ? WHERE id = ?", ).bind(&name)
+    .bind(&description)
+    .bind(&amount.to_string())
+    .bind(&circulating_date)
+        .execute(pool)
+        .await?;
+
+    Ok(())
 }
 
-pub async fn delete_recurrence(id: &Uuid) {}
+pub async fn delete_recurrence(pool: &SqlitePool, id: &Uuid) -> Result<(), Error> {
+    sqlx::query("DELETE FROM recurrences WHERE id = ?")
+        .bind(id)
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
